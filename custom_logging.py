7757b464ab35a12b22f37de4cb210290b24bceb1
Arshad Hussain <math_pal@hotmail.com>
2024-08-09 09:29:53 +0500

    Adding files Adding files

--- custom_logging.py
+++ custom_logging.py
@@ -7,25 +7,29 @@ from logging.handlers import RotatingFileHandler
 log_dir = os.path.join(os.getcwd(), 'logs')
 os.makedirs(log_dir, exist_ok=True)
 
-log_file = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+# Log file name with timestamp
+log_file = f"{datetime.now().strftime('%d_%m_%Y_%H_%M_%S')}.log"
 log_file_path = os.path.join(log_dir, log_file)
 
-#create logger
-logger=logging.getLogger()
+# Create logger
+logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-# Create a rotating file handler
-file_handler = RotatingFileHandler(log_file_path, maxBytes=10 * 1024)
+# Create handlers
+file_handler = RotatingFileHandler(log_file_path, maxBytes=10*1024*1024, backupCount=5)  # 10 MB per file
+file_handler.setLevel(logging.INFO)
+
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.INFO)
 
-formatter=logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+# Create formatter and add it to the handlers
+formatter = logging.Formatter("[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s")
 file_handler.setFormatter(formatter)
 console_handler.setFormatter(formatter)
 
+# Add handlers to the logger
 logger.addHandler(file_handler)
 logger.addHandler(console_handler)
 
-
 if __name__ == "__main__":
-    logger.info("Log file testing")
+    logger.info("Logging has started")
---
+++ src/preprocess.py
@@ -0,0 +1,60 @@
+import os
+import sys
+# Directly set the project root directory
+# project_root = "D:/DS Course/AI - Guided Projects/Guided Project - Session 4 - RAG in VS Code/RAG-Application"
+project_root = "E:/Training/Atomcamp/DS6_Bootcamp/Sessions/Guiede_Projects/mlops"
+# Ensure the project root is at the top of sys.path
+sys.path.insert(0, project_root)
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from custom_logging import logger
+from custom_exception import CustomException
+
+def load_documents(file_path: str):
+    """
+    Load documents from a PDF file.
+
+    Args:
+        file_path (str): Path to the PDF file.
+
+    Returns:
+        list: List of documents.
+    """
+    try:
+        loader = PyPDFLoader(file_path)
+        documents = loader.load()
+        logger.info("Documents loaded successfully from %s", file_path)
+        return documents
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def split_documents(documents: list, chunk_size: int = 2000, chunk_overlap: int = 400):
+    """
+    Split documents into smaller chunks.
+
+    Args:
+        documents (list): List of documents.
+        chunk_size (int): Size of each chunk.
+        chunk_overlap (int): Overlap between chunks.
+
+    Returns:
+        list: List of text chunks.
+    """
+    try:
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+        texts = text_splitter.split_documents(documents)
+        logger.info("Documents split into chunks successfully")
+        return texts
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+    
+# if __name__ == "__main__":
+#     file_path = os.path.join("data", "sample.pdf")
+#     documents = load_documents(file_path)
+#     texts = split_documents(documents)
+
+if __name__ == "__main__":
+    file_path = os.path.join("data", "sample.pdf")
+    documents = load_documents(file_path)
+    texts = split_documents(documents)
